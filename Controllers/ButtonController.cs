81fd2ba2d5eac1bf7a4ca755ec17cdd83ba91fb5
Harlee Zebley <HZebley@my.gcu.edu>
2026-05-28 14:11:59 -0400

    Initial Commit

---
+++ Controllers/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ButtonGrid.Models;
+using System.Security.Cryptography;
+
+namespace ButtonGrid.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+
+        string[] buttonImages = ["blue_button.png", "purple_button.png", "red_button.png", "yellow_button.png"];
+
+        public ButtonController()
+        {
+            //create a set of buttons with random images
+            if (buttons.Count == 0)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    int number = RandomNumberGenerator.GetInt32(0, 4);
+                    buttons.Add(new ButtonModel(i, number, buttonImages[number]));
+                }
+            }
+        }
+        public IActionResult Index()
+        {
+            return View(buttons);
+        }
+
+        public IActionResult ButtonClick(int id)
+        {
+            ButtonModel button = buttons.FirstOrDefault(b => b.Id == id);
+            if(button != null)
+            {
+                button.ButtonState = (button.ButtonState + 1) % 4;
+                button.ButtonImage = buttonImages[button.ButtonState];
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ButtonModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ButtonGrid.Models
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+        public int ButtonState { get; set; }
+        public string ButtonImage { get; set; }
+
+
+        public ButtonModel()
+        {
+
+        }
+
+        public ButtonModel(int id, int buttonState, string buttonImg)
+        {
+            Id = id;
+            ButtonState = buttonState;
+            ButtonImage = buttonImg;
+        }
+    }
+}
